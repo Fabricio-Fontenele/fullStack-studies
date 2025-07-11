5a737865cbf660c24db491c6e85d8a3fb9f6a5bb
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-07-11 01:09:24 -0300

    Add exercises for HTML structure, form handling, and comparison operators in JavaScript

--- js/exercicios/aula003/index.js
+++
@@ -1,6 +0,0 @@
-const nome = "fabricio";
-const hora = 11;
-
-console.log(
-  `ola, meu nome é ${nome} estou apredendo JavaScipt ás ${hora} da manhã`
-);
--- js/exercicios/aula008/index.js
+++
@@ -1,9 +0,0 @@
-const nome = "luiz Otavio";
-const idade = 30;
-const peso = 84;
-const altura = 1.8;
-
-let imc = peso / altura ** 2;
-
-console.log(`${nome} tem ${idade}, pesa ${peso} kg
-    tem ${altura} de altura e seu IMC é de ${imc}`);
