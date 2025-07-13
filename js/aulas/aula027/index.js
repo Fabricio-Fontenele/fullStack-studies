23580767e2bbbd99c5f624c51649745f57677aa2
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-07-13 00:35:23 -0300

    Add functions for formatting date and ensuring leading zeros in numbers

---
+++ js/aulas/aula027/index.js
@@ -0,0 +1,12 @@
+// ? : operador ternario
+// (condição) ? "valor para verdadeiro" : "valor para falso"
+
+const pontuacaoUsuario = 999;
+const nivelUsuario =
+  pontuacaoUsuario >= 1000 ? "usuario vip" : "usuario normal";
+
+// if (pontuacaoUsuario >= 1000) {
+//   console.log("usuario vip");
+// } else {
+//   console.log("usuario normal");
+// }
